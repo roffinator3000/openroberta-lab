17cdffd31e9bdadd685840f23523ae5a6179d699
Adrian Grzegorczyk <adrian.grzegorczyk@iais.fraunhofer.de>
2022-03-01 12:45:55 +0100

    IntegrationTest

--- RobotEV3/src/main/java/de/fhg/iais/roberta/visitor/validate/OrbValidatorAndCollectorVisitor.java
+++ RobotEV3/src/main/java/de/fhg/iais/roberta/visitor/validate/OrbValidatorAndCollectorVisitor.java
@@ -1,7 +1,5 @@
 package de.fhg.iais.roberta.visitor.validate;
 
-import java.util.Map;
-
 import com.google.common.collect.ClassToInstanceMap;
 
 import de.fhg.iais.roberta.bean.IProjectBean;
@@ -39,7 +37,6 @@ import de.fhg.iais.roberta.syntax.sensor.generic.TimerSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.TouchSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.UltrasonicSensor;
 import de.fhg.iais.roberta.visitor.IOrbVisitor;
-import de.fhg.iais.roberta.visitor.validate.DifferentialMotorValidatorAndCollectorVisitor;
 
 public class OrbValidatorAndCollectorVisitor extends DifferentialMotorValidatorAndCollectorVisitor implements IOrbVisitor<Void> {
 
@@ -71,7 +68,6 @@ public class OrbValidatorAndCollectorVisitor extends DifferentialMotorValidatorA
 	public Void visitCurveAction(CurveAction<Void> curveAction) {
 		return super.visitCurveAction(curveAction);
 	}
-
 
 	@Override
 	public Void visitDriveAction(DriveAction<Void> driveAction) {
